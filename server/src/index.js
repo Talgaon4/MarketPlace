a396633885d5ed2a278de38379f3b7643ce70495
Tal <talgaon4@gmail.com>
2023-06-15 21:06:10 +0300

    change cors origin

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({origin: "*"}));
 
 app.use("/auth", userRouter);
 app.use("/items", itemsRouter);
