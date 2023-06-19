dca7572109d9747d1a878b7a97261db4867b5aa5
Tal <talgaon4@gmail.com>
2023-06-19 21:19:02 +0300

    deploy test#2

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 if (procces.env.NODE_ENV === 'production') disableReactDevTools()
 
-=======
->>>>>>> 1883bfdd6d807a6a3495834fef1640814434614b
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
