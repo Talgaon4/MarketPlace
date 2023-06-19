7e6bc5c225ef1b109d3ab6d0ef08cca1ab1b47f7
Tal <talgaon4@gmail.com>
2023-06-19 21:24:24 +0300

    deploy test

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { disableReactDevTools } from '@fvilers/disable-react-devtools'
-
-if (procces.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
