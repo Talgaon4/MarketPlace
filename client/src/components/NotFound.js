6a8db44af53d5d66c5167c3b51e1700be1368e0d
Yovel Gabbay <114578333+Yovelgabay@users.noreply.github.com>
2023-06-23 18:30:28 +0300

    404 page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,11 +1,28 @@
 import React from "react";
+import moonImage from "../images/moon.png";
+import { Col, Row, Container, Image, Button } from "react-bootstrap";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
+    <Container fluid className="header-home pt-3 pb-5 px-3">
+      <Row className="pb-5">
+        <Col xs={12} sm={10}>
+          <h1 className="pb-2 pb-ml-5">
+            404
+            <br />
+            Page Not Found
+          </h1>
+        </Col>
+        <Col sm={2}>
+          <div className="astro">
+            <Image
+              className="astro-img align-self-center"
+              src={moonImage} // Use the imported image as the source
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
